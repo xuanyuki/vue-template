ad86125d5996720f048937eb77105a769f7d7016
yzy <1208374029@qq.com>
2024-11-29 17:16:40 +0800

    chore(custom): 升级vite到6.x

--- vite.config.js
+++ vite.config.js
@@ -29,14 +29,16 @@ export default defineConfig({
     preprocessorOptions: {
       scss: {
         api: 'modern-compiler',
-        // 引入覆盖el默认变量得变量文件，并且使用
-        // 您应该使用 @use 'xxx.scss' as *; 代替 @import 'xxx.scss';。因为 sass 团队说他们最终会删除 @import 语法。
-        // additionalData: `@use "@/styles/element-var.scss" as *;`,
       },
     },
   },
   server: {
     host: '0.0.0.0',
+  },
+  build: {
+    lib: {
+      cssFileName: 'style',
+    },
   },
   esbuild: {
     pure: ['console.log'],
